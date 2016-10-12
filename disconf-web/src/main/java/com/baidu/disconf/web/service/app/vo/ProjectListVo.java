fc4df778a6ca6468c067d223d6a14550685606cf
cb <cb@DESKTOP-AVNKKQD>
2016-10-12 15:57:27 +0800

    1

---
+++ disconf-web/src/main/java/com/baidu/disconf/web/service/app/vo/ProjectListVo.java
@@ -0,0 +1,28 @@
+package com.baidu.disconf.web.service.app.vo;
+
+/**
+ * @author liaoqiqi
+ * @version 2014-6-22
+ */
+public class ProjectListVo {
+
+    private long id;
+    private String projectName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+}
